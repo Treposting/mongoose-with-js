98f4d62ebf0e92581c305067d8a9a1109700678e
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-15 17:00:13 +0600

    fix:controller error as response

--- controller/testController.js
+++ controller/testController.js
@@ -2,28 +2,36 @@ const TestData = require("../model/testModel");
 const log = require("../utils/logger")
 
 module.exports.getAllDatas = async (req, res) => {
-    try{
+    try {
         const data = await TestData.find();
         res.status(200).json({
             status: "success",
             data
         });
     }
-    catch(err){
+    catch (err) {
         log.error(err);
+        res.status(500).json({
+            status: "error",
+            err: err
+        });
     }
 }
 
 module.exports.createData = async (req, res) => {
-    try{
+    try {
         const data = await TestData.create(req.body);
         res.status(201).json({
             status: "success",
             data
         });
     }
-    catch(err){
+    catch (err) {
         log.error(err);
+        res.status(500).json({
+            status: "error",
+            err: err
+        });
     }
 }
 
--- model/testModel.js
+++ model/testModel.js
@@ -1,5 +1,5 @@
 const { Schema, default: mongoose } = require("mongoose");
-
+const validator = require("validator");
 const testSchema = new Schema({
     name: {
         type: String,
@@ -8,7 +8,12 @@ const testSchema = new Schema({
     age: {
         type: Number,
         required: true,
-    }
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [validator.isEmail, "Please provide a valid email"],
+    },
 });
 
 const TestData = mongoose.model("Test", testSchema);
